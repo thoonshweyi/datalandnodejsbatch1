8ab00117c696096adc1527bdc296c8fec269394f
Thoon Shwe Yi Kyaw <thoonlay779@gmail.com>
2025-08-03 16:57:46 +0630

    3AUG

--- l5filesystemmodule/index.js
+++ l5filesystemmodule/index.js
@@ -1,5 +1,45 @@
+const { Console } = require("console");
 const fs = require("fs");
 
+// => Check File
+// => Synchronous 
+    try{
+        fs.accessSync("./assets/postone.txt",fs.constants.F_OK)
+        console.log("File exists")
+    }catch(err){
+        console.log("File does not exists.")
+    }
+
+// => Asynchronous 
+    fs.access("./assets/postone.txt",fs.constants.F_OK,(err)=>{
+        if(err){
+            console.log('File does not exists by async')
+            return;
+        }
+
+        console.log("File exists by async");
+    });
+
+// => File Info (Statistics) 
+
+fs.stat('./assets/postone.txt',(err,stats)=>{
+    if(err){
+        console.error("Error stats, ",err);
+        return;
+    }
+    console.log(stats);
+
+    console.log("File Stats ",{
+        filesize: stats.size + 'bytes',
+        createdAt: stats.birthtime,
+        updatedAt: stats.mtime,
+
+        isfile: stats.isFile(),
+        isdirectory: stats.isDirectory()
+    })
+});
+
+// => Read File
 // => Synchronous file read
 try{
     const data = fs.readFileSync("./assets/postone.txt","utf8")
@@ -20,9 +60,117 @@ fs.readFile("./assets/postone.txt",'utf-8',(err,data)=>{
 console.log("This is final call 2")
 
 
+// => Write File 
+// => Synchroous 
+const content = "This is new article.";
+try{
+    fs.writeFileSync("./datafiles/articleone.txt",content);
+    console.log("File created and written successfully");
+}catch(err){
+    console.log("Error writing file.");
+}
+
+// => Asynchronous
+fs.writeFile("./datafiles/articletwo.txt",content,()=>{
+    console.log("File created and written successfully by async");
+})
+
+// Append File 
+const newcontent = "\nAppended content text."
+try{
+    fs.appendFileSync("./datafiles/articleone.txt",newcontent);
+    console.log("File Content appended successfully.")
+}catch(err){
+    console.log("Error writing file.",err);
+}
+
+// Asynchronous
+fs.appendFile("./datafiles/articletwo.txt",newcontent,(err)=>{
+    if(err){
+        console.log("Error appending: ", err)
+    }
+     console.log("Content append successfully.")
+})
+
+// => Rename File
+// Synchronous
+try{
+    fs.renameSync("./datafiles/articleone.txt",'./datafiles/articleOne.txt');
+    console.log("File renamed successfully");
+}catch(err){
+    console.log("Error renaming file",err);
+}
+
+// Asynchronous
+fs.rename("./datafiles/articletwo.txt","./datafiles/articleTwo.txt",(err)=>{
+    if(err){
+        console.log("Error renaming file:",err);
+        return;
+    }
+    console.log("File renamed successfully by async");
+});
+
+// Delete File
+// => Synchronous
+    try{
+        if(fs.existsSync("./datafiles/articleOne.txt")){
+            fs.unlinkSync("./datafiles/articleOne.txt")
+            console.log("File deleted successfully");
+        }
+        
+    }catch(err){
+         console.log("Error deleting file:",err);
+    }
+// => Asynchronous
+    fs.unlink("./datafiles/articleTwo.txt",(err)=>{
+        if(err){
+
+            if(err.code == "ENOENT"){
+                console.log("File does not exist by async")
+            }else{
+                console.log("Error deleting file:", err);
+            }
+            return;
+        }
+        console.log("File deleted successfully by async");
+    });
+
+// Stats {
+//   dev: 2986372771,
+//   mode: 33206,
+//   nlink: 1,
+//   uid: 0,
+//   gid: 0,
+//   rdev: 0,
+//   blksize: 4096,
+//   ino: 281474976713519,
+//   size: 29,
+//   blocks: 0,
+//   atimeMs: 1753112590923.6792,
+//   mtimeMs: 1753007553093.8606,
+//   ctimeMs: 1753007553093.8606,
+//   birthtimeMs: 1752509437386.122
+// }
+
+// fs.accessSync(path, keyword)
+// fs.access(path, keyword, callback(err))
+// Note: fs.constants.F_OK = Cross-Platform consistency. way to check existence across
+
+// fs.stat(path,charcode)
 
 
-
-
-// fs.readFileSync(path,charcode)
+// fs.readFileSync(path,callback(err,data))
 // fs.readFileSync(path,charcode,callback(err,data))
+
+// fs.writeFileSync(path,content,callback)
+// fs.writeFile(path,content,callback)
+
+// fs.appendFileSync(path,newcontent)
+// fs.appendFileSync(path,newcontent,callback)
+
+//  fs.renameSync(path_old,path_new);
+//  fs.rename(path_old,path_new,callback);
+
+
+// fs.unlinkSynct(path)
+// fs.unlinkSynct(path,callback)
