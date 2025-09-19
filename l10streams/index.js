61a2c71a864853e4214af678062125e8a5bc0cf4
Thoon Shwe Yi Kyaw <thoonlay779@gmail.com>
2025-09-19 22:52:49 +0630

    19SEP

--- l10streams/index.js
+++ l10streams/index.js
@@ -1,7 +1,7 @@
 // const fs = require('fs')
-const fs = require('node:fs')
-const http = require("node:http")
-
+const fs = require('node:fs');
+const http = require("node:http");
+const path = require("node:path");
 // exe 1
 // const readstream1 = fs.createReadStream('./datafiles/postone.txt');
 
@@ -84,15 +84,16 @@ const http = require("node:http")
 // let bytes = 0;
 
 // readstream5.on('data',(chunk)=>{
-//     // console.log('length: ',chunk.length);
+//     // console.log(chunk);  // this is new announcement one
+//     // console.log('length: ',chunk.length); // 28
 
 //     bytes += Buffer.byteLength(chunk,'utf-8');
-//     console.log("Total bytes = ",bytes)
+//     console.log("Total bytes = ",bytes) // Total bytes =  28
 
 //     readstream5.pause();
 
 //     setTimeout(()=>readstream5.resume(),2000)
-// })
+// });
 
 // readstream5.on('end',()=>{
 //     console.log("Stream exe5 ended");
@@ -110,11 +111,21 @@ const http = require("node:http")
 // const ws1 = fs.createWriteStream('./datafiles/news-one.txt');
 
 // rs1.pipe(ws1);
-// ws1.on('finisih',()=>console.log("Copy Finished"));
+// ws1.on('finish',()=>console.log("Copy Finished"));
 // rs1.on('error',(err)=>console.error("Read Error",err));
 // ws1.on('error',(err)=>console.error("Write Error",err));
 
-// // exe3 (write())
+// exe2
+// const rs2 = fs.createReadStream('./datafiles/news.txt',{encoding:"utf-8"});
+// const ws2 = fs.createWriteStream('./datafiles/news-two.txt');
+
+// rs2.pipe(ws2);
+
+// ws2.on('finish',()=>console.log("Copy Finished"));
+// rs2.on('error',(err)=>console.error("Read Error",err));
+// ws2.on('error',(err)=>console.error("Write Error",err));
+
+// exe3 (write())
 // const rs3 = fs.createReadStream('./datafiles/news.txt',{encoding:"utf-8"});
 // const ws3 = fs.createWriteStream('./datafiles/news-three.txt');
 
@@ -131,20 +142,23 @@ const http = require("node:http")
 // rs3.on('error',(err)=>console.error("Read Error",err));
 // ws3.on('error',(err)=>console.error("Write Error",err));
 
-// exe4 (stream a file from )
-const rs3 = fs.createReadStream('./datafiles/news.txt',{encoding:"utf-8"});
-const ws3 = fs.createWriteStream('./datafiles/news-three.txt');
+// exe4 (stream a file to HTTP)
 
+const server = http.createServer((req,res)=>{
+    const filepath = path.join(__dirname,"datafiles/newss.txt");
+    
+    const rs4 = fs.createReadStream(filepath);
 
-rs3.on('data',(chunk)=>{
-    // console.log(chunk);
-    // console.log(chunk.toString());
+    rs4.on("open",()=>{
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        // res.writeHead(200,{"Content-Type":"application/pdf"}); // Error
+        rs4.pipe(res);
+    });
 
-    ws3.write('\n New Chunk \n')
-    ws3.write(chunk)
-})
-
-ws3.on('finisih',()=>console.log("Copy Finished"));
-rs3.on('error',(err)=>console.error("Read Error",err));
-ws3.on('error',(err)=>console.error("Write Error",err));
+    rs4.on("error",(err)=>{
+        res.writeHead(404,{"Content-Type":"text/plain"});
+        res.end(`Not Found: ${err.message}`) // Not Found: ENOENT: no such file or directory, open 'D:\datalandcouses\nodejsbatch1\l10streams\datafiles\newss.txt'
+    });
+});
+server.listen(3000,()=>console.log("server is working. http://localhost:3000"));
 
