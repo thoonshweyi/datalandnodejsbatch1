7d46ee56af7ee27fdab6bade1e404dcb71f66cc8
Thoon Shwe Yi Kyaw <thoonlay779@gmail.com>
2025-12-22 22:33:56 +0630

    22DEC

---
+++ part4/l29viewenginevariables/server.js
@@ -0,0 +1,45 @@
+// Build in middleware
+
+import express from "express";
+
+import { fileURLToPath } from "url";
+import path from "path";
+
+const app = express()
+const port = 3000
+
+// Register view engine, SET EJS as view engine 
+app.set("view engine","ejs") 
+
+
+// Set views folder 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(__dirname) // D:\datalandcouses\nodejsbatch1\part4\l27viewengine
+
+app.set("views",path.join(__dirname,"views"));
+
+// Get route
+app.get('/', (req, res) => {
+    res.render("index");
+})
+
+// => Passing Variable to EJS
+app.get('/profile', (req, res) => {
+    const user = {
+      name : "Aung Kyaw Kyaw",
+      age: 25,
+      city: "Mandalay"
+    }
+
+    res.render("profile",{user});
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
+
+// nodemon server.js  
+
+// <%= %> = output value
